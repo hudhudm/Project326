6f9f0385238982b1ab735021c028a863a8fc9ba5
NnennaEleka <101025773+NnennaEleka@users.noreply.github.com>
2024-04-05 13:57:04 -0400

    Created Traveler Class
    Init method

--- tripplanner.py
+++ tripplanner.py
@@ -2,3 +2,29 @@
 Trip Planner
 """
 
+import sys
+import argparse
+import math
+
+class Traveler:
+
+    
+    def __init__(self, name, budget = 0, trip = []):
+        """
+       This function initializes the attributes of a Traveler object.
+       Args:
+       self(Traveler) the current instance of the Traveler class
+       name(str) the name of the traveler
+       budget(float) the budget of the individual traveler
+	    trips(list) list of Trip objects under the traveler can access and edit
+
+
+       Returns: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+       """
+
+
+        self.name = name
+        self.budget = budget
+        self.trip = []
+
+
