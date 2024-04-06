6e96815f4331e753f80a0188a94d588c3c2b0898
hudhudm <165102966+hudhudm@users.noreply.github.com>
2024-04-06 14:07:43 -0400

    added trip class and methods

--- tripplanner.py
+++ tripplanner.py
@@ -7,8 +7,6 @@ import argparse
 import math
 
 class Traveler:
-
-    
     def __init__(self, name, budget = 0, trip = []):
         """
        This function initializes the attributes of a Traveler object.
@@ -21,9 +19,31 @@ class Traveler:
 
        Returns: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        """
-
-
         self.name = name
         self.budget = budget
         self.trip = []
 
+class Trip:
+    def __init__(self):
+        """
+        This function creates default destination, cost, and activities attributes.
+        """
+        self.destination = ""
+        self.cost = 0
+        self.activities = []
+
+    def addTrip(self, destination, cost, activity):
+        """
+         This function allows the user to add a trip to their traveler attribute.
+        
+        Args:
+            activity (str): the name of the activity desired at the destination
+            destination (str): the name of the destination state
+            cost (int): cost of the trip 
+        Returns:
+            str: activity, destination, and cost for the trip
+        """
+        self.activities.append(activity)
+        self.destination = destination
+        self.cost = cost
+        return f"Trip added to: {destination} with activity {activity} with a cost of ${cost}"
